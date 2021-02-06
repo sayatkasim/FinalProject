b3a846029a2300858476496db585f1a0518fa421
sayat kasim <sayat.kasim@outlook.com>
2021-02-06 23:55:52 +0300

    06.02.2021 Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,11 +11,11 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _ProductDal;
+        IProductDal _productDal;
 
         public ProductManager(IProductDal productDal)
         {
-            _ProductDal = productDal;
+            _productDal = productDal;
         }
 
         public List<Product> GetAll()
@@ -22,17 +23,22 @@ namespace Business.Concrete
             //İş kodları
             //Yetkisi var mı?
 
-            return _ProductDal.GetAll();
+            return _productDal.GetAll();
         }
 
         public List<Product> GetByUnitPrice(int id)
         {
-            return _ProductDal.GetAll(p=>p.CategoryId==id);
+            return _productDal.GetAll(p=>p.CategoryId==id);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
-            return _ProductDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<ProductDetailDto> GetProductDetail()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,13 +5,34 @@ using System;
 
 namespace ConsoleUI
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            //CategoryTest();
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var item in productManager.GetByUnitPrice(50,80))
+            foreach (var item in productManager.GetProductDetail())
+            {
+                Console.WriteLine(item.ProductName + "/" + item.CategoryName);
+            }
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var item in productManager.GetByUnitPrice(50, 80))
             {
                 Console.WriteLine(item.ProductName);
             }
